d3479ecbe8de2ead28322fbde73337b47670c53f
yenkeyke25 <34111726+yenkeyke25@users.noreply.github.com>
2021-03-20 23:11:13 -0500

    Menu and Navbar changes made

--- sites/equipmentworld.com/config/navigation.js
+++ sites/equipmentworld.com/config/navigation.js
@@ -1,11 +1,11 @@
 const topics = {
   primary: [
+    { href: '/business', label: 'Business' },
     { href: '/equipment', label: 'Equipment' },
     { href: '/better-roads', label: 'Better Roads' },
     { href: '/big-iron-dealer', label: 'Big Iron Dealer' },
-    { href: '/business', label: 'Business' },
     { href: '/technology', label: 'Technology' },
-    { href: '/workforce', label: 'Workforce' },
+    { href: '/workforce', label: 'Safety' },
   ],
   expanded: [
   ],
@@ -13,6 +13,8 @@ const topics = {
     { href: '/workforce/safety', label: 'Safety' },
     { href: '/safety-watch', label: 'Safety Watch' },
     { href: '/white-papers', label: 'White Papers' },
+    { href: '/termsandprivacy', label: 'Terms of Use' },
+
   ],
 };
 
@@ -58,7 +60,7 @@ module.exports = {
     items: [
       { href: '/termsandprivacy', label: 'Terms of User and Privacy Policy' },
       { href: '/collection', label: 'Point of Collection Notice' },
-      { href: 'https://privacyportal-cdn.onetrust.com/dsarwebform/49a9a972-547e-4c49-b23c-4cc77554cacb/cddab1bc-7e58-4eca-a20d-be42716734cf.html', label: 'Do Not Sell My Personal Information', target: '_blank' },
+      { href: 'https://www.randallreilly.com/point-of-collection-notice/', label: 'Do Not Sell My Personal Information', target: '_blank' },
       { href: '/page/contact-us', label: 'Contact Us' },
     ],
     topics: topics.primary,
