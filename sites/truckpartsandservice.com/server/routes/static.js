478405afbbd9df4e1198c81f6b9319eedfbbffe0
Brandon Krigbaum <brandonbk@gmail.com>
2021-03-26 09:17:15 -0500

    Update route for renamed template

--- sites/truckpartsandservice.com/server/routes/static.js
+++ sites/truckpartsandservice.com/server/routes/static.js
@@ -1,7 +1,7 @@
 const distributorOfTheYearAward = require('../templates/static/distributor-of-the-year-award');
-const sucessfulDealerAward = require('../templates/static/sucessful-dealer-award');
+const sucessfulDealerAwardSubmission = require('../templates/static/sucessful-dealer-award-submission');
 
 module.exports = (app) => {
   app.get('/distributor-of-the-year-award', (_, res) => { res.marko(distributorOfTheYearAward); });
-  app.get('/successful-dealer-award', (_, res) => { res.marko(sucessfulDealerAward); });
+  app.get('/successful-dealer-award-submission', (_, res) => { res.marko(sucessfulDealerAwardSubmission); });
 };
