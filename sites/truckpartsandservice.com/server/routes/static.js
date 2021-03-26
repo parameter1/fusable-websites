394cf37e5f4df117c3cead09f5660da6a3184ba9
Brandon Krigbaum <brandonbk@gmail.com>
2021-03-26 10:43:12 -0500

    Rename to nomination

--- sites/truckpartsandservice.com/server/routes/static.js
+++ sites/truckpartsandservice.com/server/routes/static.js
@@ -1,7 +1,7 @@
 const distributorOfTheYearAward = require('../templates/static/distributor-of-the-year-award');
-const sucessfulDealerAwardSubmission = require('../templates/static/sucessful-dealer-award-submission');
+const sucessfulDealerAwardNomination = require('../templates/static/sucessful-dealer-award-nomination');
 
 module.exports = (app) => {
   app.get('/distributor-of-the-year-award', (_, res) => { res.marko(distributorOfTheYearAward); });
-  app.get('/successful-dealer-award-submission', (_, res) => { res.marko(sucessfulDealerAwardSubmission); });
+  app.get('/successful-dealer-award-nomination', (_, res) => { res.marko(sucessfulDealerAwardNomination); });
 };
