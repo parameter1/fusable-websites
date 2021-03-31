4d5a06e214137ec3fa1732554b949c2473c66bf6
Josh Worden <josh@parameter1.com>
2021-03-31 11:12:46 -0500

    Add NativeX query fragments

---
+++ packages/global/graphql/fragments/native-x-story.js
@@ -0,0 +1,24 @@
+const gql = require('graphql-tag');
+const imageFragment = require('./native-x-image');
+
+module.exports = gql`
+  fragment NativeXStoryFragment on Story {
+    id
+    title
+    teaser
+    body
+    publishedAt
+    url
+    primaryImage {
+      ...NativeXImageFragment
+    }
+    advertiser {
+      id
+      name
+      logo {
+        ...NativeXImageFragment
+      }
+    }
+  }
+  ${imageFragment}
+`;
