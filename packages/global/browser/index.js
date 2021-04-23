2426f77c06c3e00d3090511d5d953afdce2d9cb8
Jacob Bare <jacob@parameter1.com>
2021-04-23 09:35:14 -0400

    Dynamically import all global components

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -4,14 +4,15 @@ import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser
 import IdentityX from '@parameter1/base-cms-marko-web-identity-x/browser';
 import NativeX from '@parameter1/base-cms-marko-web-native-x/browser';
 import OmedaRapidIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser/rapid-identify.vue';
-import BlockLoader from './block-loader.vue';
-import MenuToggleButton from './menu-toggle-button.vue';
-import NewsletterCloseButton from './newsletter-close-button.vue';
-import WufooForm from './wufoo-form.vue';
-import TopStoriesMenu from './top-stories-menu.vue';
-import CommentToggleButton from './comment-toggle-button.vue';
-import IdentityXAuthenticate from './identity-x/authenticate.vue';
-import IdentityXCommentStream from './identity-x/comments/stream.vue';
+
+const BlockLoader = () => import(/* webpackChunkName: "global-block-loader" */ './block-loader.vue');
+const MenuToggleButton = () => import(/* webpackChunkName: "global-menu-toggle-button" */ './menu-toggle-button.vue');
+const NewsletterCloseButton = () => import(/* webpackChunkName: "global-newsletter-close-button" */ './newsletter-close-button.vue');
+const WufooForm = () => import(/* webpackChunkName: "global-wufoo-form" */ './wufoo-form.vue');
+const TopStoriesMenu = () => import(/* webpackChunkName: "global-top-stories-menu" */ './top-stories-menu.vue');
+const CommentToggleButton = () => import(/* webpackChunkName: "global-comment-toggle-button" */ './comment-toggle-button.vue');
+const IdentityXAuthenticate = () => import(/* webpackChunkName: "global-identity-x-authenticate" */ './identity-x/authenticate.vue');
+const IdentityXCommentStream = () => import(/* webpackChunkName: "global-identity-x-comment-stream" */ './identity-x/comments/stream.vue');
 
 export default (Browser) => {
   GTM(Browser);
