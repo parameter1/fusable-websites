306224f06aec5808dd53777035b10fe20b3299d8
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-09 08:22:04 -0500

    Add returns

--- sites/equipmentworld.com/server/routes/redirects.js
+++ sites/equipmentworld.com/server/routes/redirects.js
@@ -1,21 +1,21 @@
 module.exports = (app) => {
   app.get('/:alias(default.asp)', (req, res) => {
     const { magid } = req.query;
-    res.redirect(301, '/');
     if (magid === '28') return res.redirect(301, '/');
+    return res.redirect(301, '/');
   });
   app.get('/:alias(apps/news/articleeqw.asp)', (req, res) => {
     const { id } = req.query;
-    res.redirect(301, '/');
     if (id === '55641') return res.redirect(301, '/');
     if (id === '62860') return res.redirect(301, '/workforce/safety');
+    return res.redirect(301, '/');
   });
   app.get('/:alias(index.php)', (req, res) => {
     const { s } = req.query;
-    if (s === 'JLG') res.redirect(301, '/14972756');
-    if (s === 'AGC') res.redirect(301, '/14972658');
     if (s === 'trench+collapse') res.redirect(301, '/workforce/safety');
-    if (s === 'rental') res.redirect(301, '/');
-    res.redirect(301, '/');
+    if (s === 'JLG') return res.redirect(301, '/14972756');
+    if (s === 'AGC') return res.redirect(301, '/14972658');
+    if (s === 'rental') return res.redirect(301, '/');
+    return res.redirect(301, '/');
   });
 };
