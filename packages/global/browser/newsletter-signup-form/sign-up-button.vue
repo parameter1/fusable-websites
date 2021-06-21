75ec911745449e38d713f4b296f9b5c62e71db06
Jacob Bare <jacob@parameter1.com>
2021-06-21 11:19:47 -0500

    Add support for overriding label

--- packages/global/browser/newsletter-signup-form/sign-up-button.vue
+++ packages/global/browser/newsletter-signup-form/sign-up-button.vue
@@ -1,5 +1,17 @@
 <template>
   <button class="btn btn-primary" type="submit">
-    Sign Up
+    {{ label }}
   </button>
 </template>
+
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Sign Up',
+    },
+  },
+};
+</script>
