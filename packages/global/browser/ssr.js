fd2bf30020707ade4eda5e3531b3aee9a839203f
Jacob Bare <jacob@parameter1.com>
2021-06-23 13:31:35 -0500

    Add newsletter menu button to SSR

--- packages/global/browser/ssr.js
+++ packages/global/browser/ssr.js
@@ -1,5 +1,7 @@
 import GlobalMenuToggleButton from './menu-toggle-button.vue';
+import GlobalSiteNewsletterMenu from './site-newsletter-menu.vue';
 
 export default {
   GlobalMenuToggleButton,
+  GlobalSiteNewsletterMenu,
 };
