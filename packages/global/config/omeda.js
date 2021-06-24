ad4dd9923ed44971880b3e4f51361f431fff54d8
Jacob Bare <jacob@parameter1.com>
2021-06-24 13:49:24 -0500

    Set omeda client key

--- packages/global/config/omeda.js
+++ packages/global/config/omeda.js
@@ -1,6 +1,6 @@
 module.exports = {
   brandKey: 'rrpcd',
-  clientKey: '',
+  clientKey: 'client_rrp',
   appId: process.env.OMEDA_APP_ID,
   inputId: process.env.OMEDA_INPUT_ID,
   graphqlUri: 'https://graphql.omeda.parameter1.com/',
