56c493d32451e2023ba75151560f91b5b6e0381e
Jacob Bare <jacob@parameter1.com>
2021-06-25 08:42:19 -0500

    Configure HWT newsletters

--- sites/hardworkingtrucks.com/config/newsletter.js
+++ sites/hardworkingtrucks.com/config/newsletter.js
@@ -5,11 +5,44 @@ const baseConfig = {
   ],
 };
 
-module.exports = {
-  signupBanner: {
-    ...baseConfig,
-    description: 'Get the business tips, industry insights and trending news every truck owner needs to know in the <em>Hard Working Trucks</em> newsletter.',
+const defaults = {
+  name: 'Don’t Miss Out',
+  description: 'Get the business tips, industry insights and trending news every truck owner needs to know in the <em>Hard Working Trucks</em> newsletter.',
+  imagePath: 'static/newsletter-pushdown/hwt-half.png',
+  defaultNewsletter: {
+    deploymentTypeId: 24,
+    name: 'Hard Working Trucks Weekly',
   },
+  newsletters: [],
+  demographic: {
+    id: 1463,
+    label: 'Your primary role?',
+    values: [
+      { id: 4924, label: 'Corporate Management/Owner' },
+      { id: 4925, label: 'Safety/Operations Management' },
+      { id: 4923, label: 'Maintenance/Equipment Mngmt' },
+      { id: 4922, label: 'Purchasing/Admin/Marketing' },
+      { id: 4929, label: 'Technician/Mechanic' },
+      { id: 4928, label: 'Sales' },
+      { id: 4927, label: 'Engineering' },
+      { id: 4926, label: 'Heavy Equipment Operator' },
+      { id: 4920, label: 'Other' },
+    ],
+  },
+};
+
+module.exports = {
+  // uses inline omeda form
+  signupBanner: {
+    ...defaults,
+  },
+  pushdown: {
+    ...defaults,
+    name: 'Newsletter Just for Truck Owners',
+    description: 'Get news you need to know about pickups, commercial vans and Class 3-8 trucks — delivered straight to your inbox.',
+  },
+
+  // links off to seperate omeda dragonform
   signupBannerLarge: {
     ...baseConfig,
     name: 'Don’t Miss Out',
@@ -19,11 +52,5 @@ module.exports = {
     ...baseConfig,
     name: 'Hardworking Trucks Daily',
     description: 'Sign up for the <span class="newsletter-name">Hardworking Trucks Daily</span> to keep up with...',
-  },
-  pushdown: {
-    ...baseConfig,
-    imagePath: 'static/newsletter-pushdown/hwt-half.png',
-    name: 'Newsletter Just for Truck Owners',
-    description: 'Get news you need to know about pickups, commercial vans and Class 3-8 trucks — delivered straight to your inbox.',
   },
 };
