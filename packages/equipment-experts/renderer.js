ed576524ed8f73a6aa4ea91b83728427d4b3a45f
Josh Worden <josh@parameter1.com>
2021-06-25 10:46:54 -0500

    Render oEmbed responses

--- packages/equipment-experts/renderer.js
+++ packages/equipment-experts/renderer.js
@@ -1,18 +1,21 @@
 const { extractEmbeddedTags } = require('@parameter1/base-cms-embedded-media');
 const buildMarkoGlobal = require('@parameter1/base-cms-marko-web/utils/build-marko-global');
 const { imageHandler, invalidHandler } = require('@parameter1/base-cms-marko-web/utils/embedded-media');
+const fetch = require('node-fetch');
 
-const fetchOembedPayload = async ({ endpoint, url, attrs }) => `
-  <!-- oEmbed ${url} via ${endpoint}: ${attrs} -->
-`;
+const { OEMBED_URI } = process.env;
 
-const oembedHandler = async (tag, $global) => {
-  const { config } = $global;
-  return fetchOembedPayload({
-    endpoint: config.oembedMountPoint(),
-    url: tag.id,
-    attrs: tag.attrs,
-  });
+const oembedHandler = async (tag) => {
+  const url = tag.id;
+  const href = `${OEMBED_URI}?url=${encodeURIComponent(url)}`;
+  try {
+    const r = await fetch(href, { credentials: 'same-origin' });
+    const { html } = await r.json();
+    return html;
+  } catch (e) {
+    // nr notice?
+    return `<!-- oEmbed ${url} start -->\n<pre>An unexpected error occurred: ${e.message}</pre>\n<!-- oEmbed ${url} end -->`;
+  }
 };
 
 module.exports = async (body, res, options) => {
