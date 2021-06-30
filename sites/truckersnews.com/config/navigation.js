b4791f8039ec369dbe4ef1b078014c4cf06a727c
Shinsina <shinsina@att.net>
2021-06-30 08:04:00 -0500

    Correct Terms of Use and Privacy Policy typo
    User -> Use

--- sites/truckersnews.com/config/navigation.js
+++ sites/truckersnews.com/config/navigation.js
@@ -56,7 +56,7 @@ module.exports = {
   },
   footer: {
     items: [
-      { href: '/termsandprivacy', label: 'Terms of User and Privacy Policy' },
+      { href: '/termsandprivacy', label: 'Terms of Use and Privacy Policy' },
       { href: '/collection', label: 'Point of Collection Notice' },
       { href: 'https://privacyportal-cdn.onetrust.com/dsarwebform/49a9a972-547e-4c49-b23c-4cc77554cacb/cddab1bc-7e58-4eca-a20d-be42716734cf.html', label: 'Do Not Sell My Personal Information', target: '_blank' },
       { href: '/page/contact-us', label: 'Contact Us' },
