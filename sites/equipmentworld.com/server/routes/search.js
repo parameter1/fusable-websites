1bde239005f8193344020ef4d2169cfb2ea43658
Brandon Krigbaum <brandonbk@gmail.com>
2021-08-03 07:22:54 -0500

    Add new-search routes

--- sites/equipmentworld.com/server/routes/search.js
+++ sites/equipmentworld.com/server/routes/search.js
@@ -1,5 +1,9 @@
+const middleware = require('@parameter1/base-cms-marko-web-search/middleware');
 const template = require('@randall-reilly/package-global/templates/search');
+const search = require('@randall-reilly/package-global/templates/search-new');
+const config = require('../../config/search');
 
 module.exports = (app) => {
   app.get('/search', (_, res) => { res.marko(template); });
+  app.get('/new-search', middleware({ config, template: search }));
 };
