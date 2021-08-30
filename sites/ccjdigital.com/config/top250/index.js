71e721a762458134d7dda987a71bd29393cd4bf7
Brandon Krigbaum <brandonbk@gmail.com>
2021-08-30 09:13:23 -0500

    Update title

--- sites/ccjdigital.com/config/top250/index.js
+++ sites/ccjdigital.com/config/top250/index.js
@@ -2,8 +2,8 @@ const all = require('./all');
 
 module.exports = {
   rootAlias: 'ccj-top-250',
-  title: 'CCJ Top 250',
   teaser: 'Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file? All of that is available completely free by providing the requested information below.',
+  title: 'CCJ Top 250: Essential data to drive your business',
   description: '',
   guides: {
     all,
