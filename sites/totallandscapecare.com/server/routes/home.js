0d78d9e4cc06d17e76bda5cc76a19eb680597154
Shinsina <shinsina@att.net>
2021-11-30 08:53:09 -0600

    Enable use of new middleware to handle redirecting

--- sites/totallandscapecare.com/server/routes/home.js
+++ sites/totallandscapecare.com/server/routes/home.js
@@ -1,10 +1,10 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
-// const postIdHandler = require('@randall-reilly/package-shared/middleware/post-id-handler');
+const postIdHandler = require('@randall-reilly/package-global/middleware/post-id-handler');
 const home = require('../templates/index');
 
 module.exports = (app) => {
-  app.get('/', withWebsiteSection({
+  app.get('/', postIdHandler({ sitePostDomain: 'wordpress.totallandscapecare.posts' }), withWebsiteSection({
     aliasResolver: () => 'home',
     template: home,
     queryFragment,
