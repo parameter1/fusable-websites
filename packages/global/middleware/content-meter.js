8243f9da0d52a2e65db03ebbb287c63353896bad
Brian Miller <brian@parameter1.com>
2022-03-01 15:12:32 -0600

    Add example shouldMeter function
    Add a shared shouldMeter function.  The shared one at the moment with exclude by  contentType, sectionId & sectionAlias.

--- packages/global/middleware/content-meter.js
+++ packages/global/middleware/content-meter.js
@@ -1,15 +1,46 @@
 const { get } = require('@parameter1/base-cms-object-path');
+const defaultValue = require('@parameter1/base-cms-marko-core/utils/default-value');
+
 const { asyncRoute } = require('@parameter1/base-cms-utils');
+const { content: loader } = require('@parameter1/base-cms-web-common/page-loaders');
+const buildContentInput = require('@parameter1/base-cms-marko-web/utils/build-content-input');
+const queryFragment = require('@randall-reilly/package-theme-monorail/graphql/fragments/content-meter');
 
 const cookieName = 'contentMeter';
 const now = new Date().getTime();
 // @todo convert this to pull from site config to allow for overrides
 const config = require('../config/content-meter');
 
-module.exports = () => asyncRoute(async (req, res, next) => {
-  if (config.enabled) {
-    const { id } = req.params;
+async function shouldMeter(req) {
+  const { apollo, params } = req;
+  const { id } = params;
+  const additionalInput = buildContentInput({ req });
+  const content = await loader(apollo, { id, additionalInput, queryFragment });
 
+  // @todo implement how the gate should be restricted
+  // By type || By section || By primarySection
+  // excludeContentTypes: Excludes content metering on page if type matches exclusions
+  const excludeContentTypes = defaultValue(config.excludeContentTypes, []);
+  if (excludeContentTypes.includes(content.type)) {
+    return false;
+  }
+  // excludePrimarySectionIds: Excludes content metering on page that matches primarySection
+  const excludePrimarySectionIds = defaultValue(config.excludePrimarySectionIds, []);
+  if (excludePrimarySectionIds.includes(content.primarySection.id)) {
+    return false;
+  }
+  // excludePrimarySectionAliass: Excludes content metering on page that matches primarySection
+  const excludePrimarySectionAliass = defaultValue(config.excludePrimarySectionAliass, []);
+  if (excludePrimarySectionAliass.includes(content.primarySection.alias)) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = () => asyncRoute(async (req, res, next) => {
+  const { id } = req.params;
+
+  if (config.enabled && await shouldMeter(req)) {
     const hasCookie = Boolean(get(req, `cookies.${cookieName}`));
 
     const value = (hasCookie) ? JSON.parse(get(req, `cookies.${cookieName}`)) : [];
