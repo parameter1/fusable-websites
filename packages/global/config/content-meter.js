d957b14f37d9000fe7c5580e2358d2ea733db36a
Brian Miller <brian@parameter1.com>
2022-03-02 14:01:35 -0600

    Comment out sample restrictions

--- packages/global/config/content-meter.js
+++ packages/global/config/content-meter.js
@@ -1,16 +1,16 @@
 module.exports = {
   enabled: true,
   viewLimit: 3,
-  excludeContentTypes: [
-    'company',
-    'contact',
-  ],
-  excludePrimarySectionIds: [
-    75347,
-  ],
-  excludePrimarySectionAlias: [
-    'home',
-  ],
+  // excludeContentTypes: [
+  //   'company',
+  //   'contact',
+  // ],
+  // excludePrimarySectionIds: [
+  //   75347,
+  // ],
+  // excludePrimarySectionAlias: [
+  //   'home',
+  // ],
   // 30 days to miliseconds
   timeframe: 30 * 24 * 60 * 60 * 1000,
 };
