fda36cd7cca31383b2f379101f2c12de252fd361
Shinsina <shinsina@att.net>
2022-03-23 12:34:28 -0500

    Correct routes, variable name and file name.
    For the updated nomination pages.

--- sites/truckpartsandservice.com/server/routes/static.js
+++ sites/truckpartsandservice.com/server/routes/static.js
@@ -1,7 +1,7 @@
 const distributorOfTheYearAward = require('../templates/static/distributor-of-the-year-award');
-const sucessfulDealerAwardNomination = require('../templates/static/sucessful-dealer-award-nomination');
+const successfulDealerAwardNomination = require('../templates/static/successful-dealer-award-nomination');
 
 module.exports = (app) => {
-  app.get('/distributor-of-the-year-award', (_, res) => { res.marko(distributorOfTheYearAward); });
-  app.get('/successful-dealer-award-nomination', (_, res) => { res.marko(sucessfulDealerAwardNomination); });
+  app.get('/distributor-of-the-year', (_, res) => { res.marko(distributorOfTheYearAward); });
+  app.get('/successful-dealer-award', (_, res) => { res.marko(successfulDealerAwardNomination); });
 };
