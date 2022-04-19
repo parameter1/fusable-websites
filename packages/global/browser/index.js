12b0fa6ade26b802b16109a435b88e7b7ccb6cf9
Brian Miller <brian@parameter1.com>
2022-04-19 09:05:35 -0500

    remove added components instead use update package
    Instead of making our own components use existing package forms.  Upgrading the package will fix the display of fields and support full width.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,18 +1,6 @@
 
 import MonoRail from '@randall-reilly/package-theme-monorail/browser';
 
-const CustomProfileComponent = () => import(/* webpackChunkName: "custome-profile-form" */ './profile.vue');
-const CustomAuthenticateComponent = () => import(/* webpackChunkName: "custome-authenticate-form" */ './authenticate.vue');
-
 export default (Browser) => {
-  MonoRail(
-    Browser,
-    {
-      enableOmedaIdentityX: true,
-      identityXComponents: {
-        CustomProfileComponent,
-        CustomAuthenticateComponent,
-      },
-    },
-  );
+  MonoRail(Browser);
 };
