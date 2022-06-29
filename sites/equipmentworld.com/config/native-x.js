85f3333c4c0dee6b797d42fc89f90d9109f4cf5a
Brandon Krigbaum <brandonbk@gmail.com>
2022-06-29 11:27:23 -0500

    Enable partner insights block

--- sites/equipmentworld.com/config/native-x.js
+++ sites/equipmentworld.com/config/native-x.js
@@ -7,6 +7,7 @@ config.enabled = true;
 config
   .setAliasPlacements('default', [
     { name: 'default', id: '6000b672140b3d0001040608' },
+    { name: 'partner-insights', id: '62bc4e2f1e84bc000139728d' },
   ]);
 
 module.exports = config;
