13ac2e38b1109508d17bcb451ad35d2de2db9036
Brian Miller <brian@parameter1.com>
2022-09-15 09:36:13 -0500

    udpate content routes with new middleware logic

--- sites/equipmentworld.com/server/routes/content.js
+++ sites/equipmentworld.com/server/routes/content.js
@@ -2,7 +2,7 @@ const withContent = require('@randall-reilly/package-global/middleware/with-cont
 const contentMeter = require('@randall-reilly/package-global/middleware/content-meter');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-page');
 const contact = require('@randall-reilly/package-global/templates/content/contact');
-const newsletterState = require('@randall-reilly/package-global/middleware/newsletter-state');
+const { newsletterState, formatContentResponse } = require('@randall-reilly/package-global/middleware/newsletter-state');
 const company = require('../templates/content/company');
 const deathByTrench = require('../templates/content/death-by-trench');
 const product = require('../templates/content/product');
@@ -54,9 +54,10 @@ module.exports = (app) => {
         queryFragment: route.queryFragment,
       }));
     } else {
-      app.get(route.regex, newsletterState(), withContent({
+      app.get(route.regex, newsletterState({ setCookie: false }), withContent({
         template: route.template,
         queryFragment: route.queryFragment,
+        formatResponse: formatContentResponse,
       }));
     }
   });
