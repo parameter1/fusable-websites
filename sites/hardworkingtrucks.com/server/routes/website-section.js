2ddd74e0d848a105a7e4cb03dc610d31c8a382b2
Brian Miller <brian@parameter1.com>
2022-09-15 09:36:58 -0500

    udpate section routes with new middleware logic

--- sites/hardworkingtrucks.com/server/routes/website-section.js
+++ sites/hardworkingtrucks.com/server/routes/website-section.js
@@ -1,14 +1,15 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
+const { newsletterState } = require('@randall-reilly/package-global/middleware/newsletter-state');
 const products = require('../templates/website-section/products');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
-  app.get('/:alias(products)', withWebsiteSection({
+  app.get('/:alias(products)', newsletterState(), withWebsiteSection({
     template: products,
     queryFragment,
   }));
-  app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
+  app.get('/:alias([a-z0-9-/]+)', newsletterState(), withWebsiteSection({
     template: section,
     queryFragment,
   }));
