3b9c6c363d972385bf2d6cd4f628a95771b38100
Brandon Krigbaum <brandon@parameter1.com>
2022-10-03 11:12:33 -0500

    Revert "Move leaderboard below the fold on mobile for OVD, CCJ"

--- sites/truckersnews.com/config/gam.js
+++ sites/truckersnews.com/config/gam.js
@@ -5,9 +5,6 @@ const config = configureGAM({ basePath: 'tn' });
 config
   .setAliasAdUnits('default', [
     { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'leaderboard' },
-    { name: 'inline-leaderboard-mobile', templateName: 'INLINE-LEADERBOARD-MOBILE', path: 'leaderboard' },
-    { name: 'leaderboard-desktop', templateName: 'LEADERBOARD-DESKTOP', path: 'leaderboard' },
-    { name: 'rotation-desktop', templateName: 'ROTATION-DESKTOP', path: 'rotation' },
     { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
     { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
     { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
@@ -16,9 +13,6 @@ config
   ])
   .setAliasAdUnits('gear', [
     { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
-    { name: 'inline-leaderboard-mobile', templateName: 'INLINE-LEADERBOARD-MOBILE', path: 'gear-leaderboard' },
-    { name: 'leaderboard-desktop', templateName: 'LEADERBOARD-DESKTOP', path: 'gear-leaderboard' },
-    { name: 'rotation-desktop', templateName: 'ROTATION-DESKTOP', path: 'gear-rotation-1' },
     { name: 'leaderboard-2', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
     { name: 'rotation', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
     { name: 'rotation-1', templateName: 'GEAR', path: 'gear-rotation-1' },
