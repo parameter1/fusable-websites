8b1b5b410899b007d5cb24206d0d2944862d2252
Brian Miller <brian@parameter1.com>
2022-10-17 09:15:10 -0500

    Add onUserProfileUpdateFormatter to add demo
    Add the `${pubCode} _profile_updated_meter` promoCode & corisponding demo onUserProfileUpdate step of registration.  Meaning if the given demo doesnt exists added it and set the promoCode.  At this point if the demo does exists do nothing, but I am guessing this will change.

--- sites/equipmentworld.com/config/identity-x-opt-in-hooks.js
+++ sites/equipmentworld.com/config/identity-x-opt-in-hooks.js
@@ -3,4 +3,10 @@ module.exports = {
     productIds: [22],
     promoCode: 'EQW_registration_meter',
   },
+  onUserProfileUpdate: {
+    appendDemographics: [
+      { demographicId: 1481, valueIds: [5134] },
+    ],
+    promoCode: 'EQW_Profile Updated_Meter',
+  },
 };
--- sites/overdriveonline.com/config/identity-x-opt-in-hooks.js
+++ sites/overdriveonline.com/config/identity-x-opt-in-hooks.js
@@ -3,4 +3,10 @@ module.exports = {
     productIds: [33],
     promoCode: 'OV_registration_meter',
   },
+  onUserProfileUpdate: {
+    appendDemographics: [
+      { demographicId: 1480, valueIds: [5132] },
+    ],
+    promoCode: 'OV_Profile Updated_Meter',
+  },
 };
