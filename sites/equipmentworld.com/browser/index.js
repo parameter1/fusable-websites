86a75af3cd1625e899a5a195ad3a2512c5ee1624
Brandon Krigbaum <brandonbk@gmail.com>
2022-11-30 13:55:17 -0600

    Disable Dun and Bradstreet

--- sites/equipmentworld.com/browser/index.js
+++ sites/equipmentworld.com/browser/index.js
@@ -1,9 +1,9 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 import Shared from '@randall-reilly/package-global/browser';
-import dunAndBradstreet from '@randall-reilly/package-global/browser/dun-and-bradstreet';
+// import dunAndBradstreet from '@randall-reilly/package-global/browser/dun-and-bradstreet';
 
 Shared(Browser);
 
-dunAndBradstreet({ debug: true }).handle({ behaviorId: '6567F8013245A7F' });
+// dunAndBradstreet({ debug: true }).handle({ behaviorId: '6567F8013245A7F' });
 
 export default Browser;
