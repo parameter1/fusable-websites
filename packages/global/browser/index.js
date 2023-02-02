092032bd095b69ae73818401b8bfc9c99183b787
Brian Miller <brian@parameter1.com>
2023-02-02 12:36:38 -0600

    Add onAuth additional event emitters for autoSignups

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -4,6 +4,15 @@ import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 
 export default (Browser) => {
+  const { EventBus } = Browser;
+  EventBus.$on('identity-x-authenticated', ({ additionalEventData }) => {
+    const { autoSignups } = additionalEventData;
+    if (autoSignups) {
+      autoSignups.forEach((autoSignup) => {
+        window.dataLayer.push({ event: 'identity-x-auto-signup', autoSignup });
+      });
+    }
+  });
   MonoRail(Browser);
   Browser.register('ContentMeterTrack', ContentMeterTrack);
 };
