b5f5178f47752114fe54bfbcdf8d41b0842b63cf
Brian Miller <brian@parameter1.com>
2023-06-08 14:18:46 -0500

    Fix check for display of mobile pushdown

--- packages/global/middleware/newsletter-state.js
+++ packages/global/middleware/newsletter-state.js
@@ -9,7 +9,7 @@ const newsletterState = ({ setCookie = true } = {}) => (req, res, next) => {
   const newsletterConfig = req.app.locals.site.getAsObject('newsletter');
   const { device } = parser(req.headers['user-agent']);
   const disableMobileCBIE = defaultValue(newsletterConfig.pushdown.disableMobileCBIE, false);
-  const canBeExpandOnMobile = disableMobileCBIE !== false || (!device || device.type !== 'mobile');
+  const disableExpandOnMobile = disableMobileCBIE && (device && device.type === 'mobile');
   const siteConfigCBIE = defaultValue(newsletterConfig.pushdown.canBeInitiallyExpanded, true);
   const hasCookie = Boolean(get(req, `cookies.${cookieName}`));
   const utmMedium = get(req, 'query.utm_medium');
@@ -17,7 +17,7 @@ const newsletterState = ({ setCookie = true } = {}) => (req, res, next) => {
   const disabled = get(req, 'query.newsletterDisabled');
   const fromEmail = utmMedium === 'email' || olyEncId || false;
   const canBeInitiallyExpanded = siteConfigCBIE && !(
-    canBeExpandOnMobile
+    disableExpandOnMobile
     || hasCookie
     || fromEmail
     || disabled
