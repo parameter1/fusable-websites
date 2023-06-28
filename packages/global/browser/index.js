8334da6995107a430eec42e2ac419d09891bdf84
Brian Miller <brian@parameter1.com>
2023-06-28 13:42:58 -0500

    Remove this as it is no longer needed
    Instead account for the conditions and fire GA4 events within GTM based on dataLayer conditions.
    
    event = identity-x-authenticate && identity-x.verifiedCount === 1 should can be added as trigger and tags fired accordingly.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,18 +1,5 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 export default (Browser) => {
-  const { EventBus } = Browser;
-  EventBus.$on('identity-x-authenticated', (data) => {
-    const {
-      id,
-      isFirstTimeVerifying,
-      source,
-      additionalEventData,
-    } = data;
-    if (isFirstTimeVerifying) {
-      const newIdentityXUser = { userId: id, source, additionalEventData };
-      window.dataLayer.push({ event: 'identity-x-created-new-user', newIdentityXUser });
-    }
-  });
   MonoRail(Browser);
 };
