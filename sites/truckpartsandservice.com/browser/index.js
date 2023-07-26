48683b99be9ef7d0d0d0b9fd2b31fb02006ce14e
Shinsina <shinsina@att.net>
2023-07-26 11:18:15 -0500

    Include Leaders Browser definition

--- sites/truckpartsandservice.com/browser/index.js
+++ sites/truckpartsandservice.com/browser/index.js
@@ -1,6 +1,8 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 import Shared from '@randall-reilly/package-global/browser';
+import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 
 Shared(Browser);
+Leaders(Browser);
 
 export default Browser;
