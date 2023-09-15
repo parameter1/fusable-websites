7d6bbc5c852d708144ca5af499e7ec07d541ba8e
Brian Miller <brian@parameter1.com>
2023-09-15 11:23:59 -0500

    Set Gam config to point at ct basePath

--- sites/cleantrucking.com/config/gam.js
+++ sites/cleantrucking.com/config/gam.js
@@ -1,6 +1,6 @@
 const configureGAM = require('@randall-reilly/package-global/config/gam');
 
-const config = configureGAM({ basePath: 'tn' });
+const config = configureGAM({ basePath: 'ct' });
 
 config
   .setAliasAdUnits('default', [
@@ -13,24 +13,6 @@ config
     { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
     { name: 'wallpaper-left', templateName: 'WALLPAPER', path: 'wallpaper-left' },
     { name: 'wallpaper-right', templateName: 'WALLPAPER', path: 'wallpaper-right' },
-  ])
-  .setAliasAdUnits('gear', [
-    { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
-    { name: 'inline-leaderboard-mobile', templateName: 'INLINE-LEADERBOARD-MOBILE', path: 'gear-leaderboard' },
-    { name: 'leaderboard-desktop', templateName: 'LEADERBOARD-DESKTOP', path: 'gear-leaderboard' },
-    { name: 'rotation-desktop', templateName: 'ROTATION-DESKTOP', path: 'gear-rotation-1' },
-    { name: 'leaderboard-2', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
-    { name: 'rotation', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
-    { name: 'rotation-1', templateName: 'GEAR', path: 'gear-rotation-1' },
-    { name: 'rotation-2', templateName: 'GEAR', path: 'gear-rotation-2' },
-    { name: 'rotation-3', templateName: 'GEAR', path: 'gear-rotation-3' },
-    { name: 'rotation-1-mobile', templateName: 'GEAR-MOBILE', path: 'gear-rotation-1' },
-    { name: 'rotation-2-mobile', templateName: 'GEAR-MOBILE', path: 'gear-rotation-2' },
-    { name: 'rotation-3-mobile', templateName: 'GEAR-MOBILE', path: 'gear-rotation-3' },
-    { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'gear-leaderboard' },
-    { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'gear-leaderboard' },
-    { name: 'wallpaper-left', templateName: 'WALLPAPER', path: 'gear-wallpaper-left' },
-    { name: 'wallpaper-right', templateName: 'WALLPAPER', path: 'gear-wallpaper-right' },
   ]);
 
 module.exports = config;
