81afd2bc9ddd979ca1fceeea5fcbdb7807fd0981
Brian Miller <brian@parameter1.com>
2023-10-03 09:11:26 -0500

    User generic RR - P1 rapidIdentProductId for CT

--- sites/cleantrucking.com/config/omeda-identity-x.js
+++ sites/cleantrucking.com/config/omeda-identity-x.js
@@ -5,6 +5,8 @@ const idxConfig = require('./identity-x');
 module.exports = configure({
   omedaConfig,
   idxConfig,
-  rapidIdentProductId: 15,
+  // this is the generic "Randall Reilly - Parameter 1", version
+  // should get update to site specific one when omeda creates it.
+  rapidIdentProductId: 130,
   websiteBehaviorAttributeId: 669712,
 });
