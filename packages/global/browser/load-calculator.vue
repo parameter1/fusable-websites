3dc147acac389e483de9cc841ae1ebea8619101e
Brian Miller <brian@parameter1.com>
2024-09-19 15:07:00 -0500

    more vue and css cleanup

--- packages/global/browser/load-calculator.vue
+++ packages/global/browser/load-calculator.vue
@@ -69,6 +69,7 @@
                   class="form-control load-calc-widget__varCostPerMile"
                   type="number"
                   min="0.00"
+                  step="0.05"
                   max="2500000"
                   :readonly="loading"
                   :placeholder="placeholder"
@@ -324,7 +325,7 @@ export default {
       // eslint-disable-next-line no-plusplus
       for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1,c.length);
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
       }
       return null;
