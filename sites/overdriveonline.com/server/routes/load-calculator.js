1e318e0f3d076fe8fcfc97fb076e4f9399678ae9
Brian Miller <brian@parameter1.com>
2024-09-23 12:10:36 -0500

    Update Load Analyzer routing

--- sites/overdriveonline.com/server/routes/load-calculator.js
+++ sites/overdriveonline.com/server/routes/load-calculator.js
@@ -4,14 +4,16 @@ const loadAnalyzer = require('@randall-reilly/package-global/templates/static/lo
 const { get } = require('@parameter1/base-cms-object-path');
 
 module.exports = (app) => {
-  app.get('/load-calculator', (_, res) => {
-    res.marko(loadCalculator);
-  });
   app.get('/load-calculator/submit', (req, res) => {
     const hasCookie = Boolean(get(req, 'cookies.loadCalculator'));
     const { payload } = hasCookie ? JSON.parse(get(req, 'cookies.loadCalculator')) : { payload: {} };
-    res.marko(loadAnalyzer,
+    res.marko(
+      loadAnalyzer,
       payload,
     );
+  });
+
+  app.get('/load-calculator', (_, res) => {
+    res.marko(loadCalculator);
   });
 };
