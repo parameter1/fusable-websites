c8ad9839979075a099cc9b78b34605034d7a8138
Shinsina <shinsina@att.net>
2024-10-10 08:30:53 -0500

    Remove PDF link for top 250

--- sites/ccjdigital.com/config/top250/index.js
+++ sites/ccjdigital.com/config/top250/index.js
@@ -3,7 +3,7 @@ const all = require('./all');
 module.exports = {
   rootAlias: 'ccj-top-250',
   title: '<em>CCJ</em> Top 250: Essential data to drive your business',
-  teaser: 'Want to sort the <em>CCJ</em> Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Click one of the buttons below to see the data sorted by haul type. Click the "All Operations" tab to see the full Top 250 listings. <a href="https://s3.us-east-2.amazonaws.com/randall-reilly-creative/sites/CCJ/2023_CCJ_Top_250_Printable_Download.pdf" target="_blank"><strong>Click here to download a printable PDF version of the CCJ Top 250.</strong></a>',
+  teaser: 'Want to sort the <em>CCJ</em> Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Click one of the buttons below to see the data sorted by haul type. Click the "All Operations" tab to see the full Top 250 listings.',
   description: '',
   guides: {
     all,
