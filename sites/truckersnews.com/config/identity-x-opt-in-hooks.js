79f8aa19fd5a35dccb0c64538525d2ce9bf3cb77
Brian Miller <brian@parameter1.com>
2024-12-06 08:11:27 -0600

    Remove TN site from this repo and into its own
    https://github.com/parameter1/randall-reilly-talent-websites

--- sites/truckersnews.com/config/identity-x-opt-in-hooks.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  onAuthenticationSuccess: {
-    productIds: [27],
-    promoCode: 'TN_registration_meter',
-  },
-};
--- sites/truckersnews.com/config/privacy-policy.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  href: 'https://randallreilly.com/legal/privacy-policy/',
-  label: 'Privacy Policy',
-  target: '_blank',
-};
